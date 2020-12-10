a90beff5dbb8011bc92e064ea020d1e4e5f72a3a
Pedro Reis <dropecosta@gmail.com>
2020-12-09 23:26:08 -0300

    Adding user registration request and body validation

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,25 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator/check');
 
-// @route   GET api/users
-// @desc    Test route
+// @route   POST api/users
+// @desc    Register user route
 // @access  Public
-router.get('/', (req, res) => res.send('User route'));
+router.post('/', [
+    check('name', 'O campo nome é obrigatório')
+        .not()
+        .isEmpty(),
+    check('email', 'O campo tem que ser um email válido')
+        .isEmail(),
+        check('password', 'O campo senha é obrigatório')
+        .isLength({ min: 6 })
+], 
+(req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    res.send('User route');
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -4,7 +4,10 @@ const connectDB = require('./config/db');
 const app = express();
 
 // connect database
-connectDB(); 
+connectDB();
+
+// init middleware
+app.use(express.json({ extended: false }));
 
 app.get('/', (req, res) => res.send('API running'));
 
