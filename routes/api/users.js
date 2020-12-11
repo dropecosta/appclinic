5149dffb7e55c8929c98f7715fb0f53d4cf7e1ee
Pedro Reis <dropecosta@gmail.com>
2020-12-11 14:26:23 -0300

    Implementing jwt

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,60 +1,73 @@
 const express = require('express');
 const router = express.Router();
-const { check, validationResult } = require('express-validator/check');
+const { check, validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 
 const User = require('../../models/User');
 
 // @route   POST api/users
 // @desc    Register user route
 // @access  Public
-router.post('/', [
-    check('name', 'O campo nome é obrigatório')
-        .not()
-        .isEmpty(),
-    check('email', 'O campo tem que ser um email válido')
-        .isEmail(),
-        check('password', 'O campo senha é obrigatório')
-        .isLength({ min: 6 })
-], 
-async (req, res) => {
+router.post(
+  '/',
+  [
+    check('name', 'O campo nome é obrigatório').not().isEmpty(),
+    check('email', 'O campo tem que ser um email válido').isEmail(),
+    check('password', 'O campo senha é obrigatório').isLength({ min: 6 }),
+    check('role', 'O campo role é obrigatório').not().isEmpty(),
+  ],
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
+      return res.status(400).json({ errors: errors.array() });
     }
 
     const { name, email, password, role } = req.body;
 
     try {
-        // see if user exists
-        let user = await User.findOne({ email });
+      // see if user exists
+      let user = await User.findOne({ email });
 
-        if (user) {
-            return res
-              .status(400)
-              .json({ errors: [{ msg: 'User already exists' }] });
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'User already exists' }] });
+      }
+
+      user = new User({
+        name,
+        email,
+        password,
+        role
+      });
+
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtToken'),
+        { expiresIn: 36000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
         }
-    
-          user = new User({
-            name,
-            email,
-            password,
-            role
-          });
-
-          const salt = await bcrypt.genSalt(10);
-
-          user.password = await bcrypt.hash(password, salt);
-
-          await user.save();
-        
-        res.send('User route');
-
+      );
     } catch (error) {
-        
+        res.status(500).send('Server error!');
     }
-
-});
-
+  }
+);
 
 module.exports = router;
