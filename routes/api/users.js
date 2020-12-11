5f6a3e240fa6e2cdf9c8d1b1294ba68f5b3e9dc7
Pedro Reis <dropecosta@gmail.com>
2020-12-11 13:01:52 -0300

    Registering user

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const { check, validationResult } = require('express-validator/check');
+const bcrypt = require('bcryptjs');
+
+const User = require('../../models/User');
 
 // @route   POST api/users
 // @desc    Register user route
@@ -14,12 +17,44 @@ router.post('/', [
         check('password', 'O campo senha é obrigatório')
         .isLength({ min: 6 })
 ], 
-(req, res) => {
+async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
     }
-    res.send('User route');
+
+    const { name, email, password, role } = req.body;
+
+    try {
+        // see if user exists
+        let user = await User.findOne({ email });
+
+        if (user) {
+            return res
+              .status(400)
+              .json({ errors: [{ msg: 'User already exists' }] });
+        }
+    
+          user = new User({
+            name,
+            email,
+            password,
+            role
+          });
+
+          const salt = await bcrypt.genSalt(10);
+
+          user.password = await bcrypt.hash(password, salt);
+
+          await user.save();
+        
+        res.send('User route');
+
+    } catch (error) {
+        
+    }
+
 });
+
 
 module.exports = router;
