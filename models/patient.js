48fabf10c66ab90e126aeb7003be034f51539241
Pedro Reis <dropecosta@gmail.com>
2021-04-17 13:35:31 -0300

    Editing pacient model

---
+++ models/patient.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const PatientSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    telephone: {
+        type: String,
+        required: true,
+    },
+    healthCare: {
+        type: String,
+        required: true,
+    },
+    date: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+module.exports = User = mongoose.model('patient', PatientSchema);
