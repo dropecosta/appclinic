57bcc15cc37666ad7c9ca8c1a20bc53cd415a023
Pedro Reis <dropecosta@gmail.com>
2021-04-18 22:06:38 -0300

    Get registry by patient ID

--- models/Registry.js
+++ models/Registry.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const RegistrySchema = new mongoose.Schema({
   patient: {
     type: mongoose.Schema.Types.ObjectId,
@@ -7,6 +7,43 @@ const RegistrySchema = new mongoose.Schema({
   description: {
     type: String,
   },
+  prescription: [
+    {
+      genericName: {
+        type: String,
+        required: true,
+      },
+      commercialName: {
+        type: String,
+        required: true,
+      },
+      producer: {
+        type: String,
+      },
+      description: {
+        type: String,
+      },
+      date: {
+        type: Date,
+        default: Date.now,
+      },
+    },
+  ],
+  exam: [
+    {
+      completed: {
+        type: Boolean,
+        default: false,
+      },
+      description: {
+        type: String,
+      },
+      date: {
+        type: Date,
+        default: Date.now,
+      },
+    },
+  ],
 });
 
-module.exports = User = mongoose.model('registry', RegistrySchema);
+module.exports = User = mongoose.model("registry", RegistrySchema);
--- routes/api/registry.js
+++ routes/api/registry.js
@@ -10,12 +10,57 @@ const Patient = require('../../models/Patient');
 // const Prescription = require('../../models/Prescription');
 // const Exam = require('../../models/Exam');
 
+// @route    POST api/registry/:id
+// @desc     Create or update registry
+// @access   Private
+router.post(
+  '/:id',
+  
+  // check('status', 'Status is required').notEmpty(),
+  // check('skills', 'Skills is required').notEmpty(),
+  async (req, res) => {
+    // const errors = validationResult(req);
+    // if (!errors.isEmpty()) {
+    //   return res.status(400).json({ errors: errors.array() });
+    // }
+
+    // destructure the request
+    const {
+      patient,
+      ...rest
+    } = req.body;
+
+    // build a profile
+    const registryFields = {
+      patient: await Patient.findById(req.params.id),
+      ...rest
+    };
+
+    try {
+      let registry = await Registry.findOneAndUpdate(
+        { patient: req.params.id },
+        { $set: registryFields },
+        { new: true, upsert: true, setDefaultsOnInsert: true }
+      );
+      console.log(registry)
+      return res.json(registry);
+    } catch (err) {
+      console.error(err.message);
+      return res.status(500).send('Server Error');
+    }
+  }
+);
+
 // @route    GET api/registry/patient
 // @desc     Get current patient registry
 // @access   Private
-router.get('/:id', auth, async (req, res) => {
+router.get('/patient/:patient_id', 
+  checkObjectId('patient_id'), 
+  async ({ params: { patient_id } }, res) => {
   try {
-    const registry = await Registry.findById(req.params.id);
+    const registry = await Registry.findOne({
+      patient: patient_id
+    }).populate('patient', ['name', 'email']);
 
     if (!registry) {
       return res.status(404).json({ msg: 'Registro não encontrado' });
